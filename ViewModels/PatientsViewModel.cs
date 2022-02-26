6e75ad3371a911565504c462519dbcd599ad9123
Ren12Bol <renat13bolurov@gmail.com>
2022-02-26 22:29:52 +0300

    Added properties

--- ViewModels/PatientsViewModel.cs
+++ ViewModels/PatientsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using LightCrm.Commands;
+using System;
 
 namespace LightCrm.ViewModels
 {
@@ -14,6 +15,73 @@ namespace LightCrm.ViewModels
                 OnPropertyChanged();
             }
         }
+
+        private string _patientName;
+        public string PatientName
+        {
+            get => _patientName;
+            set
+            {
+                _patientName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _patientSurname;
+        public string PatientSurname
+        {
+            get => _patientSurname;
+            set
+            {
+                _patientSurname = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private DateTime _date = DateTime.Today;
+        public DateTime Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _patientEmail;
+        public string PatientEmail
+        {
+            get => _patientEmail;
+            set
+            {
+                _patientEmail = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _patientSex;
+        public string PatientSex
+        {
+            get => _patientSex;
+            set
+            {
+                _patientSex = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _patientNumber;
+        public int PatientNumber
+        {
+            get => _patientNumber;
+            set
+            {
+                _patientNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
 
 
         private RelayCommand _savePatient;
