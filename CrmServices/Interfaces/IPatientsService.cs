5cd1c8d320b0315de872ebaa37c3bf889c44d0df
MrTimofeev <Pro100tim26rus@gmail.com>
2022-03-24 17:28:03 +0300

    Добавил модель пациента, добавил Dto пациента  и подключил с сервисам

---
+++ CrmServices/Interfaces/IPatientsService.cs
@@ -0,0 +1,24 @@
+﻿using CrmServices.Dtos;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace CrmServices.Services
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IPatientsService" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IPatientsService
+    {
+        [OperationContract]
+        void AddNewPatient(PatientDto patient);
+
+        [OperationContract]
+        void DeletePatient(PatientDto patient);
+
+        [OperationContract]
+        void UpdatePatient(PatientDto patient);
+
+        [OperationContract]
+        IEnumerable<PatientDto> GetPatients();
+
+    }
+}
