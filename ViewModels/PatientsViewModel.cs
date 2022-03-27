c6d5d521ba0b9c1174df74d78802580857d91974
Ren12Bol <renat13bolurov@gmail.com>
2022-03-27 17:58:35 +0300

    List of patients

--- ViewModels/PatientsViewModel.cs
+++ ViewModels/PatientsViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using LightCrm.Commands;
+using LightCrm.Models;
+using System;
+using System.Collections.ObjectModel;
 
 namespace LightCrm.ViewModels
 {
@@ -16,17 +19,68 @@ namespace LightCrm.ViewModels
         }
 
 
-        private RelayCommand _savePatient;
-        public RelayCommand SavePatient;
+        private Patient _selectedPatient;
+        public Patient SelectedPatient 
+        {
+            get => _selectedPatient;
 
-        private RelayCommand _changePatient;
-        public RelayCommand ChangePatient;
+            set
+            {
+                _selectedPatient = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Patient> Patients { get; set; }
+
+        private RelayCommand _addPatient;
+        public RelayCommand AddPatient
+        {
+            get
+            {
+                return _addPatient ??
+                    (_addPatient = new RelayCommand(obj =>
+                    {
+                        Patient patient = new Patient();
+                        Patients.Insert(0, patient);
+                        SelectedPatient = patient;
+                    }, obj => Patients.Count > 0));
+            }
+        }
+
+        private RelayCommand _savePatient;
+        public RelayCommand SavePatient
+        {
+            get
+            {
+                return _savePatient ??
+                    (_savePatient = new RelayCommand(obj =>
+                    {
+                        Patients[0] = SelectedPatient;
+                    }));
+            }
+        }
 
         private RelayCommand _deletePatient;
-        public RelayCommand DeletePatient;
+        public RelayCommand DeletePatient
+        {
+            get
+            {
+                return _deletePatient ??
+                    (_deletePatient = new RelayCommand(obj =>
+                    {
+                        Patients.Remove(SelectedPatient);
+                    },
+                    (obj) => Patients.Count > 0));
+            }
+        }
 
         public PatientsViewModel()
         {
+            SelectedPatient = new Patient();
+            Patients = new ObservableCollection<Patient>();
+            Patients.Add(SelectedPatient);
+
             Name = "Запись пациентов";
         }
     }
