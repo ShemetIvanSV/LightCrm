6321cf757bd9ee778df1c721c351b2f8317211dd
Ren12Bol <renat13bolurov@gmail.com>
2022-03-27 17:58:35 +0300

    List of patients

---
+++ Models/Patient.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using LightCrm.ViewModels;
+
+namespace LightCrm.Models
+{
+    public class Patient : BaseViewModel
+    {
+        private string _patientName;
+        public string PatientName
+        {
+            get => _patientName;
+            set
+            {
+                _patientName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _patientSurname;
+        public string PatientSurname
+        {
+            get => _patientSurname;
+            set
+            {
+                _patientSurname = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private DateTime _date = DateTime.Today;
+        public DateTime Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _patientEmail;
+        public string PatientEmail
+        {
+            get => _patientEmail;
+            set
+            {
+                _patientEmail = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _patientSex;
+        public string PatientSex
+        {
+            get => _patientSex;
+            set
+            {
+                _patientSex = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _patientNumber;
+        public int PatientNumber
+        {
+            get => _patientNumber;
+            set
+            {
+                _patientNumber = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
