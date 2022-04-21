33c5416bc6be63d79c2257cc0acb5ce4c445669a
viobelles <achmizb@yandex.ru>
2022-04-21 22:31:19 +0300

    AddDoctorsFunctionalities

---
+++ ViewModels/DoctorsViewModel.cs
@@ -0,0 +1,55 @@
+﻿using LightCrm.Commands;
+using LightCrm.Models;
+using System;
+using System.Collections.ObjectModel;
+
+namespace LightCrm.ViewModels
+{
+    public class DoctorsViewModel : BaseViewModel, IPageViewModel
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _diagnosis;   //диагноз
+        public string Diagnosis
+        {
+            get => _diagnosis;
+            set
+            {
+                _diagnosis = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _medicines;  //препараты
+        public string Medicines
+        {
+            get => _medicines;
+            set
+            {
+                _medicines = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _treatment;  //лечение
+        public string Treatment
+        {
+            get => _treatment;
+            set
+            {
+                _treatment = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
