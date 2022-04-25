bdb175633b36b39f6bff9b7d9025686ea78063e6
MrTimofeev <Pro100tim26rus@gmail.com>
2022-04-25 10:57:01 +0300

    Небольшие правки

--- Models/ModelOrder.cs
+++ Models/ModelOrder.cs
@@ -6,51 +6,9 @@ using System.Threading.Tasks;
 
 namespace LightCrm.Models
 {
-    /// <summary>
-    /// Заказы услуги
-    /// </summary>
     public class ModelOrder
     {
-        /// <summary>
-        /// Индетификатор пациента
-        /// </summary>
-        public int Id { get; set; }
-
-        /// <summary>
-        /// Имя пациента
-        /// </summary>
-        public string Patient { get; set; }
-
-        /// <summary>
-        /// Название услуги
-        /// </summary>
-        public string Servise { get; set; }
-
-        /// <summary>
-        /// Дата заказа 
-        /// </summary>
-        public DateTime Date { get; set; }
-
-        /// <summary>
-        /// Имя доктра 
-        /// </summary>
-        public string Doctor { get; set; }
-
-        /// <summary>
-        /// Проплачен заказ илм нет 
-        /// </summary>
-        public string Paid { get; set; }
-
-        /// <summary>
-        /// Статус закакза 
-        /// </summary>
-        public string Status { get; set; }
-
-        /// <summary>
-        /// Цена заказа
-        /// </summary>
-        public int Price { get; set; }
-
+      
 
     }
 }
